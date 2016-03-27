fe20461284658cf2fe30aa6841944d74a13ff5cf
Jesse Maes <kebertyx@gmail.com>
2016-03-27 00:49:53 -0600

    Improve image generation program

--- thematrix.py
+++ thematrix.py
@@ -2,8 +2,12 @@
 
 import sys
 import math
-from PIL import Image, ImageDraw, ImageFont
+import getopt
+import tempfile
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
+WIDTH, HEIGHT = 800, 600
+# WIDTH, HEIGHT = 1920, 1080
 
 def width(bbox):
     return bbox[2] - bbox[0]
@@ -17,32 +21,44 @@ def shift(bbox, x, y):
     bbox[2] += x
     bbox[3] += y
 
-def hackertext(text, output="output.png"):
-    font = ImageFont.truetype("/usr/share/fonts/TTF/DroidSansMono.ttf", 92)
+def hackertext(text):
+    font = ImageFont.truetype("/usr/share/fonts/TTF/DroidSansMono.ttf", 64)
     bbox = list(font.getmask(text).getbbox())
 
     # Calculate a scaled up size for the image
-    k = max(math.ceil(width(bbox) / 800),
-            math.ceil(height(bbox) / 600))
-    w = 800 * k
-    h = 600 * k
+    k = max(math.ceil(width(bbox) / WIDTH),
+            math.ceil(height(bbox) / HEIGHT))
+    w = WIDTH * k
+    h = HEIGHT * k
+    # Create parameters based on how small the text will appear
+    bright = k * 256 // (k + 1)
+    blur = math.ceil(1.5 * k)
+    print('>>>', k, file=sys.stderr) # debugging info
 
     # Create a black background image
     image = Image.new('RGB', (w, h), 0x000000)
     draw = ImageDraw.Draw(image)
-
     # Move the bounding box to the center of the image
-    shift(bbox, w // 2 - width(bbox) // 2, h // 2 - height(bbox) // 2)
+    shift(bbox, (w - width(bbox)) // 2, (h - height(bbox)) // 2)
+    # Draw the text in green on the image and resize it to WIDTHxHEIGHT
+    draw.text((bbox[0], bbox[1]), text, (0, bright, 0), font=font)
+    image = image.filter(ImageFilter.GaussianBlur(blur))
+    image = image.resize((WIDTH, HEIGHT), Image.ANTIALIAS)
 
-    # Draw the text in green on the image and resize it to 800x600
-    draw.rectangle(bbox, outline=0xff)
-    draw.text((bbox[0], bbox[1]), text, (0, 120, 0), font=font)
-    image = image.resize((800, 600), Image.ANTIALIAS)
-    image.save(output)
+    return image
 
 def main():
     if (len(sys.argv) > 1):
-        hackertext('<{}>'.format(' '.join(sys.argv[1:])))
+        opts, args = getopt.getopt(sys.argv[1:], 'o:ih')
+        opts = dict(opts)
+
+        outfile = opts.get('-o', tempfile.mktemp('.png', 'img_'))
+        text = input() if '-i' in opts else ' '.join(args)
+
+        image = hackertext('<{}>'.format(text))
+        image.save(outfile)
+
+        print(outfile)
 
 if __name__ == '__main__':
     main()
